2d3c40a73aff8c2c127139c70bb115ee1a7071cb
Rajat Lavekar <rajat.lavekar@gmail.com>
2023-04-07 16:13:24 +0530

    Adding the index files

---
+++ Home_1.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HMS</title>
+  <link rel="stylesheet" href="home.css">
+  <h2><b>Confirm Your Identity : </b></h2><br><br><br><br><br><br>
+</head>
+
+<body>
+  
+
+  <ul>
+    <li><a href="Patient.php" data-text="Patient">Patients</a></li>
+    <li><a href="Doctor.php" data-text="Doc">Doctors</a></li>
+    <li><a href="Employee.php" data-text="Employee">Employees</a></li>
+    <li><a href="admin_list.php" data-text="Admin">Administrators</a></li>
+    <li><a href="Us.php" data-text="Us">About Us</a></li>
+  </ul>
+</body>
+
+</html>
